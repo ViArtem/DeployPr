ef0a1a739c76c0931868e75d44b2d0f46586aea0
ViArtem <110330436+ViArtem@users.noreply.github.com>
2023-01-08 18:06:53 +0200

    'addCSS'

--- connnect.js
+++ connnect.js
@@ -2,12 +2,12 @@ import db from 'mongoose'
 import dotev from 'dotenv'
 dotev.config()
 const url = process.env.URL
-//db.set('strictQuery', false);
+db.set('strictQuery', false);
 
 //Database connection
 async function conn() {
     try {
-       await db.connect(url)  //{readPreference: 'secondaryPreferred'}
+       await db.connect(url) 
     } catch (e) {
         console.log(e);
     }
--- server.js
+++ server.js
@@ -1,56 +1,54 @@
-import { createRequire } from 'node:module';
+import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
-import  express from 'express'
-import {conn} from './connnect.js'
-const path = require('path')
+import express from "express";
+import { conn } from "./connnect.js";
+const path = require("path");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "hbs");
 // app.use(express.static(path.resolve( "public")))
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
+let allMiddleware = [
+  routerdel,
+  routerss,
+  router,
+  routerer,
+  express.static(path.resolve("public")),
+];
 
-
-let allMiddleware = [routerdel, routerss, router, routerer,express.static(path.resolve( "public"))]
-
-for (let i = 0; i < allMiddleware.length; i++) { 
-
-    app.use(allMiddleware[i])
-    
+for (let i = 0; i < allMiddleware.length; i++) {
+  app.use(allMiddleware[i]);
 }
 
-
-import Person  from  './models/users.js'
+import Person from "./models/users.js";
 
 //Delete
-import {routerdel} from './Routes/delete.js'  //booleanDeleteUser
+import { routerdel } from "./Routes/delete.js"; //booleanDeleteUser
 
-
-//Adding users  
-import {routerss} from './Routes/add.js'
- 
+//Adding users
+import { routerss } from "./Routes/add.js";
 
 //Find users
-import {router, findUserBoolean, foundUser } from './Routes/find.js'
-
-
+import { router, findUserBoolean, foundUser } from "./Routes/find.js";
 
 //Update
-import { routerer } from './Routes/edit.js';
-
- 
+import { routerer } from "./Routes/edit.js";
 
 // Render home page
-app.get('/', async(req, res)=>{
-    const arrayUsersNames = await Person.find({}).lean()
+app.get("/", async (req, res) => {
+  const arrayUsersNames = await Person.find({}).lean();
 
-    res.render(path.resolve('view','index.hbs'), {arrayUsersNames, findUserBoolean, foundUser }) 
-})
+  res.render(path.resolve("view", "indexN.hbs"), {
+    arrayUsersNames,
+    findUserBoolean,
+    foundUser,
+  });
+});
 
-
-app.listen(PORT, ()=>{
-    console.log('Started...');
-    conn()
-})
+app.listen(PORT, () => {
+  console.log("Started...");
+  conn();
+});
